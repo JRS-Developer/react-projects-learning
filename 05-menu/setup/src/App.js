9ad8750bd654b68894d03474d958977b6a9451a8
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-06-19 17:49:24 -0400

    Listo el proyecto de menu de restaurante

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,10 +1,35 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+// Awesome
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
-  return <h2>menu project setup</h2>;
+	const [menuItems, setMenuItems] = useState(items);
+	const [categories, setCategories] = useState(allCategories);
+
+	const filterItems = (category) => {
+		if (category === "all") {
+			setMenuItems(items);
+			return;
+		}
+		const newItems = items.filter((item) => item.category === category);
+		setMenuItems(newItems);
+	};
+	return (
+		<main>
+			<section className="menu section">
+				<div className="titlle">
+					<h2>Our menu</h2>
+					<div className="underline"></div>
+				</div>
+			</section>
+			<Categories filterItems={filterItems} categories={categories} />
+			<Menu items={menuItems} />
+		</main>
+	);
 }
 
 export default App;
