b76586ffde11f7d8aac70cec7e352f1be0750575
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-06-23 00:51:42 -0400

    Listo el proyecto de generador de colores

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from 'react';
+import rgbToHex from './utils';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ index, rgb, weight, DivideColorsBy }) => {
+	const [alert, setAlert] = useState(false);
+	const bcg = rgb.join(',');
+	const hex = rgbToHex(...rgb);
 
-export default SingleColor
+	useEffect(() => {
+		let timer = setTimeout(() => {
+			setAlert(false);
+		}, 2000);
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [alert]);
+
+	return (
+		<article
+			onClick={() => {
+				setAlert(true);
+				navigator.clipboard.writeText(hex);
+			}}
+			className={`color ${index > DivideColorsBy && 'color-light'}`}
+			style={{
+				backgroundColor: `rgb(${bcg})`,
+			}}
+		>
+			<p className='percent-value'>{weight}%</p>
+			<p className='color-value'>{hex}</p>
+			{alert && <p className='alert'>copied to clipboard</p>}
+		</article>
+	);
+};
+
+export default SingleColor;
